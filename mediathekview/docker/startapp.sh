2d1b6f25118fa40e4356fa68b4460ea2583854cd
Daniel Wydler <daniel@wydler.eu>
2026-03-14 23:11:45 +0100

    Add set -eux for safer script execution

--- mediathekview/docker/startapp.sh
+++ mediathekview/docker/startapp.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# Setze sichere Shell-Optionen:
+# -e: bricht das Script sofort ab, falls ein Fehler auftritt
+# -u: Fehler bei unset Variablen vermeiden
+# -x: jeden ausgeführten Befehl ausgeben (hilfreich für Debugging)
+set -eux
+
 #
 export HOME=/config
 
