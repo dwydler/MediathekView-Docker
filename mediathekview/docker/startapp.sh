b93a9a285eaaa56721baf9177d25c53a3b497742
Daniel Wydler <daniel@wydler.eu>
2026-03-14 23:12:53 +0100

    Added more comments

--- mediathekview/docker/startapp.sh
+++ mediathekview/docker/startapp.sh
@@ -6,8 +6,8 @@
 # -x: jeden ausgeführten Befehl ausgeben (hilfreich für Debugging)
 set -eux
 
-#
+# Setze HOME auf das persistente Konfigurationsverzeichnis im Container
 export HOME=/config
 
-#
+# Starte MediathekView und nutze das HOME-Verzeichnis für Konfiguration
 /opt/MediathekView/MediathekView -m $HOME
