2156c54be7f5c2fa9fa8be70fec0997fc050ab3a
Daniel Wydler <daniel@wydler.eu>
2026-03-15 22:45:59 +0100

    Replace sed with usermod

--- rootfs/etc/cont-init.d/90-mediathekview.sh
+++ rootfs/etc/cont-init.d/90-mediathekview.sh
@@ -39,7 +39,7 @@ fi
 
 # Setze das Home-Verzeichnis des Users "app" von /dev/null auf /output,
 # damit der Benutzer ein gültiges Home-Verzeichnis im Volume erhält
-sed -i 's#^\(app:[^:]*:[^:]*:[^:]*:[^:]*:\)/dev/null#\1/output#' /etc/passwd
+usermod -d /output/ app
 
 #
 # Rechte setzen
